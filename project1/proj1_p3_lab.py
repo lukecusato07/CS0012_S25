964f5d389f00c3b9304ae4764b329be6d9ad12cc
Luke Cusato <133079492+lukecusato07@users.noreply.github.com>
2025-02-13 14:20:29 -0500

    Adding feb 10 lab examples

---
+++ project1/proj1_p3_lab.py
@@ -0,0 +1,72 @@
+from random import random
+
+'''
+- if, elif, and else
+
+2 Loops:
+1.) for: count controlled --> run for a specified num of times
+2.) while: condition controlled --> True or False
+
+- incrementation
+- print
+
+'''
+x = 5
+for i in range(10):
+    x += 2
+    print(x)
+
+# start: incl, end: excl
+for i in range(1,101):
+    print(x)
+'''
+format:
+
+for _ range(number):
+    do this
+
+for students in cs0012
+
+
+'''
+
+# while loops
+age = 21
+
+while age == 21 or age > 21:
+    print("You are old enough to enter!")
+
+print("welcome")
+
+# Another example
+userAge = int(input("Please enter your age for entry: "))
+
+while userAge < 21:
+    print("You are not old enough to enter this bar.")
+    # Prompting the next person in line
+    userAge = int(input("Please enter your age for entry: "))
+
+
+'''
+format:
+
+while condition:
+    do this
+
+'''
+
+'''
+format:
+variable += ammount (number)
+
+addition: +=
+subtraction: -=
+multiply: *=
+divide: /=
+'''
+
+
+
+
+
+
