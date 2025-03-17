63029b63b43126386102c614f4df8631c0fad140
Luke Cusato <133079492+lukecusato07@users.noreply.github.com>
2025-03-17 12:42:46 -0400

    Update test.py

--- project2/part2/test.py
+++ project2/part2/test.py
@@ -1,20 +1,20 @@
 URL:https://www.nytimes.com/2016/04/18/article1
-this is the first article for april 15 starting week 0
+This is the first --article, for april 15 starting week 0!!
 URL:https://www.nytimes.com/2016/04/20/article2
-this article falls in week 0 as well
+This article!! falls in week 0; as well!!
 URL:https://www.nytimes.com/2016/04/25/article3
-this article should be in week 1
+This! article should be in week 1....??
 URL:https://www.nytimes.com/2016/05/05/article4
-testing for the first article in may which should be in week 2
+Testing for the first article, in May --which should be in week 2.
 URL:https://www.nytimes.com/2016/05/15/article5
-testing an article for mid-may which should fall in week 3
+Testing an article for mid-May, which should fall in week 3!!!??
 URL:https://www.nytimes.com/2016/05/22/article6
-another article to test the week 3 list
+Another article to test, the week 3 list....!
 URL:https://www.nytimes.com/2016/06/01/article7
-this article is at the start of june and should fall in week 6
+This article -is, the start of June and should fall in week 6!!
 URL:https://www.nytimes.com/2016/06/07/article8
-article for the second week of june which should be in week 7
+Article? for the second week of June, which should be in week 7!!
 URL:https://www.nytimes.com/2016/06/15/article9
-this article is mid-june and should be in week 8
+This article is mid-June, and should be in week 8???!
 URL:https://www.nytimes.com/2016/06/30/article10
-this article is the end of june falling into week 10
+This article! is the end of June, falling into week 10!!
